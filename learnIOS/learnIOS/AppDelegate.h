54a9fc5f332c1da8dd0bfa6b7e3039d0f70cc689
yuhaichen <yuhaichen@qiyi.com>
2019-04-13 09:42:21 +0800

    add ios project

---
+++ learnIOS/learnIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learnIOS
+//
+//  Created by Mac on 2019/4/13.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learnIOS/learnIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learnIOS
+//
+//  Created by Mac on 2019/4/13.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
